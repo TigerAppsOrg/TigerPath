7ebd253c5186744f16197eeb131b73ee978bccc3
Richard Chu <richardchu10@gmail.com>
2018-03-31 20:11:42 -0400

    Add CAS authentication

--- tigerpath/urls.py
+++ tigerpath/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # app urls
     path('', views.index, name='index'),
     path('landing', views.landing, name='landing'),
     path('about', views.about, name='about'),
+    # cas auth
+    path('login', views.login, name='cas_ng_login'),
+    path('logout', views.logout, name='cas_ng_logout'),
 ]
