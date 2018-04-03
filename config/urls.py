3532724152cd47e6c8c8ff95b0ec0207eb02facb
Richard Chu <richardchu10@gmail.com>
2018-04-03 02:42:48 -0400

    Add docker support, remove django-heroku

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('', include('tigerpath.urls')),
-    path('admin/', admin.site.urls),
 ]
+
+if settings.ADMIN_ENABLED:
+    urlpatterns.append(path('admin/', admin.site.urls))
