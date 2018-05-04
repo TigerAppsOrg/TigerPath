c0e2ad3d95ee47ba66268438978b3185fd730eb3
Barak Nehoran <bnehoran@hotmail.com>
2018-05-04 22:30:36 +0000

    Made file paths in tester.py robust to moving the folder.

--- tigerpath/majors-and-certificates/scripts/tester.py
+++ tigerpath/majors-and-certificates/scripts/tester.py
@@ -8,8 +8,9 @@ import filecmp
 
 import verifier
 
-SCHEMA_LOCATION = "schema.json" # path to the requirements JSON schema
-TESTS_LOCATION = "verifier_tests" # folder where the test files are stored
+dir_path = os.path.dirname(os.path.realpath(__file__)) # the directory containing this file
+SCHEMA_LOCATION = os.path.join(dir_path, "schema.json") # path to the requirements JSON schema
+TESTS_LOCATION = os.path.join(dir_path, "verifier_tests") # folder where the test files are stored
 
 def _json_format(obj):
    return json.dumps(obj, sort_keys=False, indent=2, separators=(',', ': ')) + "\n"
@@ -25,7 +26,7 @@ def main():
                 year = int(f.readline())
                 courses = json.loads(f.read())
             major_filename = major_name + "_" + str(year)  + ".json"
-            major_filepath = os.path.join(verifier.MAJORS_LOCATION, major_filename)
+            major_filepath = os.path.join(dir_path, verifier.MAJORS_LOCATION, major_filename)
             with open(major_filepath, 'r') as f:
                 major = json.load(f)
             with open(SCHEMA_LOCATION, 'r') as s:
