a1a7d4bdcc7f9678b0e3442a41f1075122d9c5f3
Adeniji Ogunlana <adeniji.ogunlana@gmail.com>
2018-05-13 20:03:08 -0400

    Logout Dropdown (#83)
    * moved logout button to dropdown

---
+++ tigerpath/static/app/js/dropdown.js
@@ -0,0 +1,18 @@
+function dropFunction() {
+    document.getElementById("netid-btn").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
