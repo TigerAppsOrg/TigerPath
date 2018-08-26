9c74a35dd5b06dd71f93d8f3bfed0d1520840d33
danielleung27 <danielleung27@yahoo.com>
2018-08-26 11:49:31 -0700

    fixed popovers for left

--- frontend/src/Requirements.js
+++ frontend/src/Requirements.js
@@ -127,7 +127,8 @@ export function addReqPopovers(){
       html: true,
       animation: true,
       placement: 'right',
-      container: 'body'
+      container: 'body',
+      template: '<div class="popover req-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
     })
     .on('mouseenter', function(){
       var _this = this;
