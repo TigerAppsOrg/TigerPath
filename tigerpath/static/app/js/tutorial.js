dcad9d519b5305a1e1138bc1112fa50efe8aacc4
Richard Chu <richardchu10@gmail.com>
2018-09-12 19:34:34 -0700

    Update onboarding tutorial

--- tigerpath/static/app/js/tutorial.js
+++ tigerpath/static/app/js/tutorial.js
@@ -7,15 +7,12 @@ function startIntro() {
         intro: "Welcome to TigerPath! We'll take you on a brief tour to show you how to use the app. Click \"Next\" to begin."
       },
       {
-        element: '#search-courses',
-        intro: `You can search for courses you want to add to your schedule here.
-        Go ahead and search your department code and press enter.`,
-        position: 'right'
-      },
-      {
-        element: '#display-courses',
-        intro: `Search results will appear here.
+        element: '#search-pane',
+        intro: `You can search for courses you want to add to your schedule here. This can be by the course code or title.
+        Try searching something!
+        <br><br>
         Fall courses appear in <b style="color: purple;">purple</b>, Spring courses appear in <b style="color: #217bbe;">blue</b>, and courses that are offered in both semesters appear in <b style="color: #e17b15;">orange</b>.
+        <br><br>
         You can drag courses from the search results to any semester in your schedule.`
       },
       {
@@ -25,12 +22,15 @@ function startIntro() {
       {
         element: '#requirements',
         intro: `Each course that you've added to your schedule will automatically show up underneath the requirements that it can satisfy.
+        <br><br>
         If a course can only satisfy one of multiple requirements, it will show up underneath each of those requirements and be grayed out.
-        You can click on a grayed out course to make it count for that requirement.`
+        You can click on a grayed out course to make it count for that requirement.
+        <br><br>
+        You can also hover over a requirement to read more information about it, and to search for courses that satisfy that requirement!`
       },
       {
         element: '#netid-btn',
-        intro: "If you would like to change your major or modify other settings, you can click here and then on \"Settings\"."
+        intro: "If you would like to change your major, add a nickname, or modify other settings, you can click here and then on \"Settings\"."
       },
       {
         element: '#help-btn',
