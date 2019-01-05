c4ea020c941093370c6c1d2ee6e91a5b4d16e8f2
Richard Chu <churichard@users.noreply.github.com>
2019-01-04 18:23:57 -0800

    Optimize scraper, add logging, refactor code (#172)

--- tigerpath/management/commands/tigerpath_get_courses.py
+++ tigerpath/management/commands/tigerpath_get_courses.py
@@ -1,14 +1,7 @@
-import json
-
-from tigerpath.scraper import scrape_all
-from django.conf import settings
-from django.core.cache import caches
 from django.core.management.base import BaseCommand
-
+from tigerpath.scraper import scrape_all
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
         scrape_all.get_all_courses()
         self.stdout.write('course selection: courses scraped successfully')
-        
