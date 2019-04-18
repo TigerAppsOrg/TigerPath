24b7e7fb53a0ab07580a15ba04b817f4fb156703
danielleung27 <danielleung27@yahoo.com>
2019-04-18 13:18:01 -0400

    Improve AP credits discoverability (#243)
    Improve AP credits discoverability

--- frontend/src/components/MainView.js
+++ frontend/src/components/MainView.js
@@ -72,7 +72,7 @@ export default class MainView extends Component {
       <React.Fragment>
         <Nav>
           <NavButton active={scheduleTabActive} onClick={(e) => this.setTab(TABS.SCHEDULE_TAB, e)}>Schedule</NavButton>
-          <NavButton active={externalCreditsTabActive} onClick={(e) => this.setTab(TABS.EXTERNAL_CREDITS_TAB, e)}>External Credits</NavButton>
+          <NavButton active={externalCreditsTabActive} onClick={(e) => this.setTab(TABS.EXTERNAL_CREDITS_TAB, e)}>AP/External Credits</NavButton>
         </Nav>
         <Content>
           {scheduleTabActive &&
