a8f38a3bdc5378c46c28afcde611224398ca42fd
bnehoran <bnehoran@users.noreply.github.com>
2019-09-09 01:43:58 -0400

    Revert "Add course info and eval links to course popovers (#261)"
    This reverts commit d5194b0614d34534a59bc9fab5c4a871311a3e99.

--- frontend/src/Popover.js
+++ frontend/src/Popover.js
@@ -1,9 +1,5 @@
 import $ from 'jquery';
-import { getSemesterType, isFallSemester, isSpringSemester, convertSemToTermCode } from 'utils/SemesterUtils';
-
-// TODO: refactor from here and SearchCard.js
-const BASE_COURSE_OFFERINGS_URL = 'https://registrar.princeton.edu/course-offerings/course_details.xml';
-const BASE_COURSE_EVAL_URL = 'https://reg-captiva.princeton.edu/chart/index.php';
+import { getSemesterType, isFallSemester, isSpringSemester } from 'utils/SemesterUtils';
 
 export function addPopover(course, courseKey, semIndex) {
   let courseName = course['name'];
@@ -32,22 +28,6 @@ export function addPopover(course, courseKey, semIndex) {
   } else if (courseSemType === 'spring' && isFallSemester(getSemesterType(semIndex))) {
     content += "<div class='popover-warning'>This course has previously only been offered in the Spring.</div>";
   }
-
-  let courseId = course["id"];
-  // TODO: Don't hardcode the semester list
-  let courseSemList = course["semester_list"];
-  let termCode = convertSemToTermCode(courseSemList[courseSemList.length - 1]);
-  let courseInfoLink = BASE_COURSE_OFFERINGS_URL + "?courseid=" + courseId + "&term=" + termCode;
-  let courseEvalLink = BASE_COURSE_EVAL_URL + "?terminfo=" + termCode + "&courseinfo=" + courseId;
-
-  content += "<div className='search-card-links'>"
-  content += "<a href=" + courseInfoLink + " target='_blank' rel='noopener noreferrer'> "
-  content += "<i class='fas fa-info-circle fa-lg fa-fw course-info'></i>"
-  content += "</a> "
-  content += "<a href=" + courseEvalLink + " target='_blank' rel='noopener noreferrer'>"
-  content += "<i class='fas fa-chart-bar fa-lg fa-fw course-eval' />"
-  content += "</a>"
-  content += "</div>"
 
   courseElement.attr("data-content", content);
 
