b7e8e7eafb43392ad59e32813a47fcffca71777f
Richard Chu <richardchu10@gmail.com>
2020-07-15 12:39:40 -0700

    Remove service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import App from 'components/App';
 import 'styles/Courses.css';
 import 'styles/Requirements.css';
@@ -8,4 +7,3 @@ import 'styles/Print.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-registerServiceWorker();
