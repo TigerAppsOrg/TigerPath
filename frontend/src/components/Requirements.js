aeabf7210174a480085e87dce2fb1c569e4ea121
Richard Chu <churichard@users.noreply.github.com>
2020-07-16 16:06:48 -0700

    Fix find satisfying courses button in req popover (#350)

--- frontend/src/components/Requirements.js
+++ frontend/src/components/Requirements.js
@@ -71,6 +71,7 @@ export default class Requirements extends Component {
           boundary: 'viewport',
           template:
             '<div class="popover req-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>',
+          sanitize: false,
         })
         .on('mouseenter', () => {
           reqLabelElement.popover('show');
