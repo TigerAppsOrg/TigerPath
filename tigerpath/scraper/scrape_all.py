062bc93c987d07769241d65dccf7e3be9f5d95bd
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-11-03 14:08:57 -0400

    Scrape only most recent term

--- tigerpath/scraper/scrape_all.py
+++ tigerpath/scraper/scrape_all.py
@@ -4,34 +4,33 @@ from tigerpath.scraper import scrape_parse, scrape_validate, scrape_import
 
 def get_all_courses():
     # we can generate these given settings.CURR_TERM
-    term_codes = settings.ACTIVE_TERMS
-    for term_code in term_codes:
-        try:
-            print("Scraping for semester " + str(term_code))
-            courses = scrape_parse.scrape_parse_semester(term_code)
+    term_code = settings.ACTIVE_TERMS[-1]
+    try:
+        print("Scraping for semester " + str(term_code))
+        courses = scrape_parse.scrape_parse_semester(term_code)
 
-            print('Validating courses')
-            for course in courses:
-                scrape_validate.validate_course(course) # just a sanity check in case we ever modify scrape_parse
+        print('Validating courses')
+        for course in courses:
+            scrape_validate.validate_course(course) # just a sanity check in case we ever modify scrape_parse
 
-            print('Updating database')
-            scrape_counter = scrape_import.ScrapeCounter()
-            length = len(courses)
-            for index, course in enumerate(courses):
-                print('Updating course {} of {}'.format(index+1, length))
-                for _ in range(3):
-                    try:
-                        scrape_import.scrape_import_course(course, scrape_counter)
-                    except Exception as e:
-                        connection.connection.close()
-                        connection.connection = None
-                        print('Updating course {} failed, retrying...'.format(course))
-                        print(e)
-                    else:
-                        break
+        print('Updating database')
+        scrape_counter = scrape_import.ScrapeCounter()
+        length = len(courses)
+        for index, course in enumerate(courses):
+            print('Updating course {} of {}'.format(index+1, length))
+            for _ in range(3):
+                try:
+                    scrape_import.scrape_import_course(course, scrape_counter)
+                except Exception as e:
+                    connection.connection.close()
+                    connection.connection = None
+                    print('Updating course {} failed, retrying...'.format(course))
+                    print(e)
                 else:
-                    print('Updating course {} failed'.format(course))
+                    break
+            else:
+                print('Updating course {} failed'.format(course))
 
-            print(str(scrape_counter))
-        except Exception as e:
-            raise e
+        print(str(scrape_counter))
+    except Exception as e:
+        raise e
