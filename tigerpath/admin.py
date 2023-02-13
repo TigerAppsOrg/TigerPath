948b3167eded8813da9ff3b7c835095b2f0caea9
Nicholas Padmanabhan <nicholaspad@gmail.com>
2023-02-13 17:10:51 -0500

    Add models to admin interface

--- tigerpath/admin.py
+++ tigerpath/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from tigerpath.models import (
+    Course,
+    Course_Listing,
+    Major,
+    Meeting,
+    Professor,
+    Section,
+    Semester,
+    UserProfile,
+)
+
 # Register your models here.
+admin.site.register(Semester)
+admin.site.register(Professor)
+admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(Meeting)
+admin.site.register(Course_Listing)
+admin.site.register(Major)
+admin.site.register(UserProfile)
