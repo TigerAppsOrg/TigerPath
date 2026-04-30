750eade9186fb3de43da5e112b5f7577ae46081f
Angelina Ji <aj2126@princeton.edu>
2026-04-29 23:49:30 -0400

    Use ManifestStaticFilesStorage to fix missing staticfiles manifest

--- config/settings/base.py
+++ config/settings/base.py
@@ -152,7 +152,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
     },
 }
 
